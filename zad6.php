c6562e960a3b66c3183e2b4ee823230655ec2f96
IvailoH <ivailovr10@abv.bg>
2015-06-23 01:27:01 +0300

    Create zad6.php

---
+++ zad6.php
@@ -0,0 +1,10 @@
+<?php
+$movies = array (
+ 'movie1' => "Jason",
+ 'movie2' => "Jackie",
+ 'movie3' => "Zahari",
+ 'movie4' => "Martin",
+ 'movie5' => "Jack");
+ echo "The main actors of the movie are: {$movies['movie1']}</br>";
+ echo "The main actors of the movie are: {$movies['movie2']}";
+?>
